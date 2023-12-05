226085be87a0e425a7c4e0161029f9f4a44651b7
김대운 <eodnseodns23@gmail.com>
2023-12-05 23:13:26 +0900

    css mian body 추가

--- 목소리변환/목소리_변환페이지_김대운/turning_voice.js
+++ 목소리변환/목소리_변환페이지_김대운/turning_voice.js
@@ -1,4 +1,19 @@
-function remove_shadow() {
-  let freebutton = document.getElementById("start-free");
-  freebutton.classList.add("clicked");
-}
+document.getElementById("start_free").addEventListener("click", () => {
+  document.getElementById("start_free").style.boxShadow = "none";
+  setTimeout(() => {
+    document.getElementById("start_free").style.boxShadow =
+      "0px 4px 4px rgba(0, 0, 0, 0.25)";
+  }, 80);
+});
+
+document.getElementById("start_free").addEventListener("click", () => {
+  location.reload();
+});
+
+document.getElementById("check_voice").addEventListener("click", () => {
+  document.getElementById("check_voice").style.boxShadow = "none";
+  setTimeout(() => {
+    document.getElementById("check_voice").style.boxShadow =
+      "0px 4px 4px rgba(0, 0, 0, 0.25)";
+  }, 120);
+});
