c754a4acbfa3da2a3028c06cb324e4b52a245eb6
김대운 <eodnseodns23@gmail.com>
2023-12-05 17:30:50 +0900

    초기화

--- 목소리변환/목소리_변환페이지_김대운/turning_voice.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("logo").addEventListener("click", function () {
-  history.back();
-});
-document.getElementById("start-free").addEventListener("click", function () {
-  this.classList.add("clicked");
-  setTimeout(() => {
-    this.classList.remove("clicked");
-  }, 100);
-});
